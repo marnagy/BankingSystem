a2cb7b29fcdb46e6718d5fa285759e81d0f5a8ed
Marek Nagy <62247071@cuni.cz>
2020-04-07 12:14:26 +0200

    looks like needed commit...

--- Client/src/ClientSession.java
+++ Client/src/ClientSession.java
@@ -2,28 +2,28 @@ import java.io.*;
 import java.net.Socket;
 
 public final class ClientSession {
-    private final Socket socket;
-    final String host;
-    final int port;
-    Long sessionID = null;
-    public ClientSession() throws IOException {
-        host = "localhost";
-        port = 5000;
-        socket = new Socket(host, port);
-    }
+	private final Socket socket;
+	final String host;
+	final int port;
+	Long sessionID = null;
+	public ClientSession() throws IOException {
+		host = "localhost";
+		port = 5000;
+		socket = new Socket(host, port);
+	}
 
-    public OutputStream getOutputStream() throws IOException {
-        return socket.getOutputStream();
-    }
-    public InputStream getInputStream() throws IOException {
-        return socket.getInputStream();
-    }
-    public void close() throws IOException {
-        socket.close();
-    }
+	public OutputStream getOutputStream() throws IOException {
+		return socket.getOutputStream();
+	}
+	public InputStream getInputStream() throws IOException {
+		return socket.getInputStream();
+	}
+	public void close() throws IOException {
+		socket.close();
+	}
 
-    public Object getID(ObjectInput oi) throws IOException {
-        sessionID = oi.readLong();
-        return sessionID;
-    }
+	public Object getID(ObjectInput oi) throws IOException {
+		sessionID = oi.readLong();
+		return sessionID;
+	}
 }
