7d4df5c6936dfde66a3c50ac1540b51152792ae7
Marek Nagy <62247071@cuni.cz>
2020-05-04 09:31:04 +0200

    Added more user-friendly error when server doesn't receive good arguments from command line

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-    	var master = MasterServerSession.getDefault();
-    	master.run(args[0], args[1].toCharArray());
+    	if (args.length == 2 &&
+			    Pattern.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",args[0])
+    		&& args[1] != ""){
+		    var master = MasterServerSession.getDefault();
+		    master.run(args[0], args[1].toCharArray());
+	    }
+    	else{
+    		System.out.println("Invalid arguments received.");
+	    }
     }
 }
